2b58aa9d3b2065c60765a86accd7e1570abfab25
elliscapp <ecapp@ohs.stanford.edu>
2017-10-01 15:41:14 -0700

    Support for Linkage Clamps
    Added support for a two-servo clamping mechanism with a 4-bar linkage.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModeMecanum.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModeMecanum.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 /**
  * Created by tarunsingh on 9/24/17.
@@ -15,6 +16,9 @@ public class OpModeMecanum extends LinearOpMode {
     private DcMotor lfDrive;
     private DcMotor rrDrive;
     private DcMotor rfDrive;
+    private DcMotor suction;
+    private Servo leftClamp; // port 1 on the servo controller
+    private Servo rightClamp; // port 2 on the servo controller
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -25,12 +29,32 @@ public class OpModeMecanum extends LinearOpMode {
             double leftStickX = gamepad1.left_stick_x;
             double leftStickY = gamepad1.left_stick_y;
             double rightStickX = gamepad1.right_stick_x;
+            boolean aButton = gamepad1.a;
+            boolean dPadUp = gamepad1.dpad_up;
+            boolean dPadDown = gamepad1.dpad_down;
+            boolean dPadLeft = gamepad1.dpad_left;
+            boolean dPadRight = gamepad1.dpad_right;
 
             double[] motorPowers = calcMotorPowers(leftStickX, leftStickY, rightStickX);
             lrDrive.setPower(motorPowers[0]);
             lfDrive.setPower(motorPowers[1]);
             rrDrive.setPower(motorPowers[2]);
             rfDrive.setPower(motorPowers[3]);
+
+            if(aButton)
+                suction.setPower(1);
+            else
+                suction.setPower(0);
+
+            if(dPadUp)
+                leftClamp.setPosition(leftClamp.getPosition()+0.01);
+            else if(dPadDown)
+                leftClamp.setPosition(leftClamp.getPosition()-0.01);
+
+            if(dPadRight)
+                rightClamp.setPosition(rightClamp.getPosition()+0.01);
+            else if(dPadLeft)
+                rightClamp.setPosition(rightClamp.getPosition()-0.01);
 
             telemetry.addData("Left Stick X", leftStickX);
             telemetry.addData("Left Stick Y", -leftStickY);
@@ -51,12 +75,16 @@ public class OpModeMecanum extends LinearOpMode {
         lfDrive = hardwareMap.dcMotor.get("lfDrive");
         rrDrive = hardwareMap.dcMotor.get("rrDrive");
         rfDrive = hardwareMap.dcMotor.get("rfDrive");
+        suction = hardwareMap.dcMotor.get("suction");
+        leftClamp = hardwareMap.servo.get("leftClamp");
+        rightClamp = hardwareMap.servo.get("rightClamp");
 
         //Set directions
         lrDrive.setDirection(DcMotor.Direction.REVERSE);
         lfDrive.setDirection(DcMotor.Direction.REVERSE);
         rrDrive.setDirection(DcMotor.Direction.FORWARD);
         rfDrive.setDirection(DcMotor.Direction.FORWARD);
+        suction.setDirection(DcMotor.Direction.FORWARD);
     }
 
     private double[] calcMotorPowers(double leftStickX, double leftStickY, double rightStickX) {
