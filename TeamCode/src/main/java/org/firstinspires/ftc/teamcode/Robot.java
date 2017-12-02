299d6cdad2510ddfc5c0843c07bdcb7675569e32
Tarun <tsingh2@ohs.stanford.edu>
2017-12-01 16:21:57 -0800

    Added all hardware + subsystems to Robot class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
@@ -7,42 +9,86 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-import org.firstinspires.ftc.teamcode.subsystems.*;
+import org.firstinspires.ftc.teamcode.subsystems.Drive;
+import org.firstinspires.ftc.teamcode.subsystems.Glyft;
+import org.firstinspires.ftc.teamcode.subsystems.Jewel;
+import org.firstinspires.ftc.teamcode.subsystems.RelicRecovery;
 /**
  * Created by AndrewC on 11/25/2017.
  */
 
 public class Robot {
     public String name;
-    public HardwareMap hardwareMap;
-    public ElapsedTime timer;
+    private HardwareMap hardwareMap;
+    private ElapsedTime timer;
 
-    public DcMotorEx frontLeft;
-    public DcMotorEx frontRight;
-    public DcMotorEx rearRight;
-    public DcMotorEx rearLeft;
+    //DC Motors
+    private DcMotorEx frontLeftDriveMotor;
+    private DcMotorEx frontRightDriveMotor;
+    private DcMotorEx rearRightDriveMotor;
+    private DcMotorEx rearLeftDriveMotor;
+    private DcMotorEx leftGlyftMotor;
+    private DcMotorEx rightGlyftMotor;
+    private DcMotorEx relicMotor;
 
-    public Servo jewelServo;
+    //Servos
+    private Servo jewelServo;
+    private Servo leftSqueezerServo;
+    private Servo rightSqueezerServo;
+    private Servo relicWristServo;
+    private Servo relicClawServo;
 
-    public ColorSensor colorSensor;
+    //Sensors
+    private BNO055IMU imu;
+    private ColorSensor colorSensor;
 
+    //Subsystems
+    public Drive drive;
+    public Glyft glyft;
+    public RelicRecovery relicRecovery;
     public Jewel jewel;
 
     public Robot(OpMode opMode, ElapsedTime timer){
         hardwareMap = opMode.hardwareMap;
         this.timer = timer;
+        init();
     }
     public void init(){
-        frontRight = (DcMotorEx) hardwareMap.dcMotor.get("FR");
-        frontLeft = (DcMotorEx) hardwareMap.dcMotor.get("FL");
-        rearRight = (DcMotorEx) hardwareMap.dcMotor.get("RR");
-        rearLeft = (DcMotorEx) hardwareMap.dcMotor.get("RL");
+        //DC Motors
+        frontLeftDriveMotor = (DcMotorEx) hardwareMap.dcMotor.get("fl_drive");
+        frontRightDriveMotor = (DcMotorEx) hardwareMap.dcMotor.get("fr_drive");
+        rearLeftDriveMotor = (DcMotorEx) hardwareMap.dcMotor.get("rl_drive");
+        rearRightDriveMotor = (DcMotorEx) hardwareMap.dcMotor.get("rr_drive");
+        leftGlyftMotor = (DcMotorEx) hardwareMap.dcMotor.get("l_glyft");
+        rightGlyftMotor = (DcMotorEx) hardwareMap.dcMotor.get("r_glyft");
+        relicMotor = (DcMotorEx) hardwareMap.dcMotor.get("relic");
 
-        jewelServo = hardwareMap.servo.get("JS");
-        colorSensor = hardwareMap.colorSensor.get("CS");
+        //Servos
+        jewelServo = hardwareMap.servo.get("jewel_servo");
+        leftSqueezerServo = hardwareMap.servo.get("left_squeezer");
+        rightSqueezerServo = hardwareMap.servo.get("right_squeezer");
+        relicWristServo = hardwareMap.servo.get("wrist_relic");
+        relicClawServo = hardwareMap.servo.get("claw_relic");
+
+        //Sensors
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        colorSensor = hardwareMap.colorSensor.get("color");
+
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
+        parameters.loggingEnabled      = true;
+        parameters.loggingTag          = "IMU";
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+
+        imu.initialize(parameters);
+
+        //Subsystems
+        drive = new Drive(frontLeftDriveMotor, frontRightDriveMotor, rearLeftDriveMotor, rearRightDriveMotor, imu, timer);
+        glyft = new Glyft(leftSqueezerServo, rightSqueezerServo, leftGlyftMotor, rightGlyftMotor, timer);
+        relicRecovery = new RelicRecovery(relicWristServo, relicClawServo, relicMotor, timer);
         jewel = new Jewel(jewelServo, colorSensor, timer);
-
-
     }
 }
 
