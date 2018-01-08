c01bf1735cd80e9f34f68b3369f42aa261c41f02
Tarun <tsingh2@ohs.stanford.edu>
2018-01-07 16:44:58 -0800

    Glyft changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Glyft.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Glyft.java
@@ -6,11 +6,8 @@ import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.ServoImplEx;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-import org.firstinspires.ftc.teamcode.Robot;
-
-import static com.qualcomm.robotcore.hardware.DcMotor.*;
-import static com.sun.tools.doclint.HtmlTag.P;
-import static org.firstinspires.ftc.teamcode.subsystems.Glyft.GlyftState.STOPPED;
+import static com.qualcomm.robotcore.hardware.DcMotor.RunMode;
+import static org.firstinspires.ftc.teamcode.subsystems.Glyft.GlyftState.MANUAL;
 
 /**
  * Created by Cat on 11/10/17.
@@ -22,13 +19,15 @@ public class Glyft {
     private static final double VS_RIGHT_OPEN_POSITION = 0.40;
     private static final double VS_LEFT_CLOSED_POSITION = 0.15;
     private static final double VS_RIGHT_CLOSED_POSITION = 0.55;
+    private static final double VS_LEFT_SCORING_POSITION = 0.30;
+    private static final double VS_RIGHT_SCORING_POSITION = 0.45;
 
     private static final double CW_LEFT_OPEN_POSITION = 0.20;
-    private static final double CW_RIGHT_OPEN_POSITION = 0.60;
+    private static final double CW_RIGHT_OPEN_POSITION = 0.50;
     private static final double CW_LEFT_INTAKING_POSITION = 0;
     private static final double CW_RIGHT_INTAKING_POSITION = 0.9;
-    private static final double CW_LEFT_STORED_POSITION = 0.45;
-    private static final double CW_RIGHT_STORED_POSITION = 0.10;
+    private static final double CW_LEFT_STORED_POSITION = 0.80;
+    private static final double CW_RIGHT_STORED_POSITION = 0.00;
 
     public Servo squeezerLeft;
     public Servo squeezerRight;
@@ -41,10 +40,10 @@ public class Glyft {
     public ElapsedTime OpModeTime;
 
     public enum GlyftState {
-        INTAKING, LIFTING, STACKING, SCORING, STOPPED
+        INTAKING, LIFTING, GRABBING, STOPPED, MANUAL, SCORING
     }
 
-    public GlyftState glyftState = STOPPED;
+    public GlyftState glyftState = MANUAL;
 
     public Glyft(Servo squeezerLeft, Servo squeezerRight, DcMotor glyftMotor1, DcMotor glyftMotor2, ServoImplEx intakePivotLeft, ServoImplEx intakePivotRight, CRServo compliantWheelLeft, CRServo compliantWheelRight, ElapsedTime OpModeTime) {
         this.squeezerLeft = squeezerLeft;
@@ -58,7 +57,7 @@ public class Glyft {
         this.OpModeTime = OpModeTime;
     }
 
-    // TO DO: specific functions + positions + test
+    // TODO: specific functions + positions + test
     public GlyftState getGlyftState() {
         return glyftState;
     }
@@ -67,10 +66,12 @@ public class Glyft {
         glyftState = state;
     }
 
+    /*
     public void rotateSqueezers() {
         squeezerLeft.setPosition(1); // self-reminder: servo goes from 0 to 1, 0.5 is 45 deg turn
         squeezerRight.setPosition(1);
     }
+    */
 
     public void openSqueezers() {
         squeezerLeft.setPosition(VS_LEFT_OPEN_POSITION );
@@ -103,25 +104,39 @@ public class Glyft {
         compliantWheelRight.setPower(power);
     }
 
+    public void setCWLeftPower(double power) {
+        compliantWheelLeft.setPower(power);
+    }
+
+    public void setCWRightPower(double power) {
+        compliantWheelRight.setPower(power);
+    }
+
     public void moveGlyft (int targetPosition)
     {
-        glyftMotor1.setMode(RunMode.STOP_AND_RESET_ENCODER);
-        glyftMotor2.setMode(RunMode.STOP_AND_RESET_ENCODER);
+        //glyftMotor1.setMode(RunMode.STOP_AND_RESET_ENCODER);
+        //glyftMotor2.setMode(RunMode.STOP_AND_RESET_ENCODER);
         glyftMotor1.setMode(RunMode.RUN_TO_POSITION);
         glyftMotor2.setMode(RunMode.RUN_TO_POSITION);
         glyftMotor1.setTargetPosition(targetPosition);
         glyftMotor2.setTargetPosition(targetPosition);
-        glyftMotor1.setPower(0.60);
-        glyftMotor2.setPower(0.60);
-        while (glyftMotor1.isBusy() && glyftMotor2.isBusy())
-        {
-            //do nothing
-        }
+        glyftMotor1.setPower(1.0);
+        glyftMotor2.setPower(1.0);
     }
 
     public void setPower(double power) {
         glyftMotor1.setPower(power); // need to test
         glyftMotor2.setPower(power);
+    }
+
+    public void setRunMode(RunMode runMode) {
+        glyftMotor1.setMode(runMode);
+        glyftMotor2.setMode(runMode);
+    }
+
+    public void scoreSqueezers() {
+        squeezerLeft.setPosition(VS_LEFT_SCORING_POSITION);
+        squeezerRight.setPosition(VS_RIGHT_SCORING_POSITION);
     }
 
 }
